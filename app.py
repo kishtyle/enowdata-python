c2f03f81f8aa58b2625de4fb172ab46a2ffc55bc
kishtyle <30485517+kishtyle@users.noreply.github.com>
2020-04-12 11:25:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,61 +122,61 @@ def facedetect():
         face_names = []
         process_this_frame = True
         # Grab a single frame of video
-            ret, frame = video_capture.read()
+        ret, frame = video_capture.read()
             
-            # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
             
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
             
-            # Only process every other frame of video to save time
-            if process_this_frame:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        # Only process every other frame of video to save time
+        if process_this_frame:
+           # Find all the faces and face encodings in the current frame of video
+           face_locations = face_recognition.face_locations(rgb_small_frame)
+           face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
                 
-                face_names = []
-                for face_encoding in face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding, 0.7)
-                    # name = "Unknown"
+           face_names = []
+           for face_encoding in face_encodings:
+               # See if the face is a match for the known face(s)
+               matches = face_recognition.compare_faces(known_face_encodings, face_encoding, 0.7)
+               # name = "Unknown"
                     
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    #if True in matches:
-                    #	first_match_index = matches.index(True)
-                    #	name = known_face_names[first_match_index]
+               # # If a match was found in known_face_encodings, just use the first one.
+               #if True in matches:
+               #	first_match_index = matches.index(True)
+               #	name = known_face_names[first_match_index]
                     
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        if best_match_index == 0:
-                            name = known_face_names[best_match_index]
-                            print('first')
-                            resp = jsonify( {
-                            u'status': 200,
-                            u'Name': name
-                            } )
-                        else:
-                            name = known_face_names[best_match_index-1]
-                            print('Not first')
-                            resp = jsonify( {
-                            u'status': 200,
-                            u'Name': name
-                            } )
-                    else:
-                        name = "Unknown"
-                        # if matches[best_match_index]:
-                        #     if best_match_index > 0:
-                        #         name = known_face_names[best_match_index-1]
-                        resp = jsonify( {
-                        u'status': 200,
-                        u'Name': name
-                        } )
+               # Or instead, use the known face with the smallest distance to the new face
+               face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+               best_match_index = np.argmin(face_distances)
+               if matches[best_match_index]:
+                  if best_match_index == 0:
+                      name = known_face_names[best_match_index]
+                      print('first')
+                      resp = jsonify( {
+                      u'status': 200,
+                      u'Name': name
+                      } )
+                   else:
+                      name = known_face_names[best_match_index-1]
+                      print('Not first')
+                      resp = jsonify( {
+                      u'status': 200,
+                      u'Name': name
+                      } )
+               else:
+                  name = "Unknown"
+                  # if matches[best_match_index]:
+                  #     if best_match_index > 0:
+                  #         name = known_face_names[best_match_index-1]
+                  resp = jsonify( {
+                  u'status': 200,
+                  u'Name': name
+                  } )
                             
-                    face_names.append(name)
-                    return resp
+               face_names.append(name)
+               return resp       
         process_this_frame = not process_this_frame
             
         resp = jsonify( {
